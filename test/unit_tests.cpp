c5a324383c363ecfa0c7df15dfb658aa51c09043
SorenHolmP <sorenholmpetersen@gmail.com>
2020-09-20 10:58:38 +0200

    Added tests for FiveGuessAI. Must guess code in 5 or less guesses

--- test/unit_tests.cpp
+++ test/unit_tests.cpp
@@ -4,6 +4,8 @@
 //#include <mastermind.hpp>
 #include <mastermindgame.hpp>
 #include <mastermindlogic.hpp>
+#include <fiveguessai.hpp>
+#include <cmath>
 
 
 TEST(LogicTest, evaluateGuessTest)
@@ -36,6 +38,43 @@ TEST(LogicTest, guessIsValidTest)
     EXPECT_FALSE(guessIsValid("rrrr", valid_colors, 7));
 }
 
+
+
+TEST(LogicTest, getCombinationsTest)
+{
+    std::string valid_colors = "123456";
+    int code_length = 4;
+    std::list<std::string> combinations;
+    getAllPossibleCombinations(combinations, valid_colors, code_length, "");
+
+    EXPECT_EQ(combinations.size(), std::pow(valid_colors.length(), code_length)); // e.g. combinations.size() == 1296, for 6 colors, and code_len = 4
+
+}
+
+
+TEST(PlayerTest, FiveGuessAI)
+{
+    srand(time(NULL));
+    int code_length = 4;
+    std::string colors = "123456";
+
+    FiveGuessAI fg(colors, code_length);
+    std::string code = fg.makeCode();
+    BWresult latest_result(-1,-1);
+
+    int guess_count = 0;
+    std::string guess;
+
+    while(latest_result.blacks != code_length)
+    {
+        guess = fg.makeGuess(latest_result);
+        latest_result = evaluateGuess(guess, code);
+        guess_count++;
+    }
+
+    EXPECT_EQ(guess, code);
+    EXPECT_LE(guess_count, 5);
+}
 
 
 
